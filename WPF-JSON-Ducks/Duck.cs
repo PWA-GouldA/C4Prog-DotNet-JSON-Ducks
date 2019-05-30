79ac5762a0c1c26a5b5097a3e21cf9f7caa0301d
Adrian Gould (Lecturer) <adrian.gould@nmtafe.wa.edu.au>
2019-05-30 17:30:47 +0800

    Initial Commit: Mostly Complete

---
+++ WPF-JSON-Ducks/Duck.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.ComponentModel;
+using System.Windows.Media;
+using WPF_JSON_Ducks;
+
+namespace WPF_JSON_Ducks
+{
+    enum DuckSizes
+    {
+        Error = -1,
+        Egg = 0,
+        Duckling = 1,
+        Small = 2,
+        Medium = 3,
+        Large = 4,
+        ExtraLarge = 5,
+        Supersized = 6,
+        Ermagerd = 7
+    }
+
+    public class ColourInfo : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public string ColourName { get; set; }
+        public Color Colour { get; set; }
+
+        public SolidColorBrush SampleBrush
+        {
+            get { return new SolidColorBrush(Colour); }
+        }
+        public string HexValue
+        {
+            get { return Colour.ToString(); }
+        }
+
+        public ColourInfo(string colour_name, Color colour)
+        {
+            ColourName = colour_name;
+            Colour = colour;
+        }
+    }
+
+    class Duck
+    {
+        private string _name;
+        private string _colour;
+        private int _birthYear;
+        private DuckSizes _size;
+
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        public string Colour
+        {
+            get { return _colour; }
+            set { _colour = value; }
+        }
+
+        public SolidColorBrush ColourSample
+        {
+            get {
+                SolidColorBrush xx = new SolidColorBrush(
+                    (Color)ColorConverter.ConvertFromString(Colour)
+                    );
+                return xx;
+            }
+        }
+
+        public DuckSizes Size
+        {
+            get { return _size; }
+            set { _size = value; }
+        }
+
+        public int BirthYear
+        {
+            get { return _birthYear; }
+            set { _birthYear = value; }
+        }
+        public Duck()
+        {
+            Name = "";
+            Colour = "";
+            Size = DuckSizes.Error;
+            BirthYear = 0;
+        }
+
+        public Duck(string name, string colour, DuckSizes size, int birthYear)
+        {
+            this.Name = name;
+            this.Colour = colour;
+            Size = size;
+            this.BirthYear = birthYear;
+        }
+
+        public Duck(string name, Color colour, DuckSizes size, int birthYear)
+        {
+            this.Name = name;
+            this.Colour = colour.ToString();
+            Size = size;
+            this.BirthYear = birthYear;
+        }
+
+        private DuckSizes ConvertToDucksize(string size)
+        {
+            DuckSizes duckSized;
+            Enum.TryParse<DuckSizes>(size, out duckSized);
+            return duckSized;
+        }
+
+
+        // override public string ToString() {
+        //  return Name + " (" + Colour + ") " + SizeText();
+        // }
+
+        override public string ToString() => Name + " (" + Colour + ") " + Enum.GetName(typeof(DuckSizes), Size);
+
+
+    }
+}
--- WPF-JSON-Ducks/MainWindow.xaml.cs
+++ WPF-JSON-Ducks/MainWindow.xaml.cs
@@ -1,17 +1,36 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+
+/// <summary>
+/// JSON Data save and Load demo
+/// 
+/// Demo contains following:
+///     JSON Serialisation
+///     List of Objects
+///     JSON Save
+///     JSON Load
+///     Combo box with HTML colour names, samples and Hex codes
+///     Add to list
+///     
+/// Resources used:
+///     https://www.newtonsoft.com/json
+///     http://csharphelper.com/blog/2015/10/list-colors-in-wpf-and-c/
+///     http://flounder.com/csharp_color_table.htm
+///     https://stackoverflow.com/questions/32489262/how-to-fill-wpf-combobox-with-text-image-at-runtime
+///     http://www.blackwasp.co.uk/StatusBar.aspx
+///     https://www.wpf-tutorial.com/common-interface-controls/menu-control/
+///     https://www.wpf-tutorial.com/listview-control/listview-data-binding-item-template/
+///     https://www.wpf-tutorial.com/common-interface-controls/statusbar-control/
+///     
+/// </summary>
 
 namespace WPF_JSON_Ducks
 {
@@ -20,9 +39,126 @@ namespace WPF_JSON_Ducks
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        string duckFileName = "ducks.json";
+
+        List<Duck> ducks = new List<Duck>();
+
         public MainWindow()
         {
             InitializeComponent();
+            AddColours();
+            AddSizes();
+            AddSampleDucks();
         }
+
+        void LoadDucks()
+        {
+            ducks.Clear();
+            // deserialize JSON directly from a file
+            using (StreamReader file = File.OpenText(duckFileName))
+            {
+                JsonSerializer serializer = new JsonSerializer();
+                ducks = (List<Duck>)serializer.Deserialize(file, typeof(List<Duck>));
+            }
+            DuckListView.ItemsSource = ducks;
+        }
+
+        void SaveDucks()
+        {
+            // serialize JSON to a string and then write string to a file
+            //File.WriteAllText(@duckFileName, JsonConvert.SerializeObject(ducks));
+
+            // serialize JSON directly to a file
+            using (StreamWriter file = File.CreateText(@duckFileName))
+            {
+                JsonSerializer serializer = new JsonSerializer();
+                serializer.Serialize(file, ducks);
+            }
+        }
+
+        private void NameTextbox_SelectionChanged(object sender, RoutedEventArgs e)
+        {
+            int row = NameTextbox.GetLineIndexFromCharacterIndex(NameTextbox.CaretIndex);
+            int col = NameTextbox.CaretIndex - NameTextbox.GetCharacterIndexFromLineIndex(row);
+            CursorPositionLabel.Text = "Line " + (row + 1) + ", Char " + (col + 1);
+        }
+
+        void AddSampleDucks()
+        {
+            if (!File.Exists(duckFileName))
+            {
+
+                ducks.Add(new Duck()
+                {
+                    Name = "Yellow Duck",
+                    Colour = "Yellow",
+                    Size = DuckSizes.Egg
+                }
+            );
+                //ducks.Add(new Duck() { Name = "Green Duck", Colour = "Green", Size = DuckSizes.Duckling });
+                //ducks.Add(new Duck() { Name = "Cyan Duck", Colour = "Cyan", Size = DuckSizes.Small });
+                //ducks.Add(new Duck() { Name = "Magenta Duck", Colour = "Magenta", Size = DuckSizes.Medium });
+                //ducks.Add(new Duck() { Name = "Blue Duck", Colour = "Blue", Size = DuckSizes.Large });
+                //ducks.Add(new Duck() { Name = "Red Duck", Colour = "Red", Size = DuckSizes.Supersized });
+                //ducks.Add(new Duck() { Name = "Purple Duck", Colour = "Purple", Size = DuckSizes.Ermagerd });
+                //ducks.Add(new Duck() { Name = "Black Duck", Colour = "Black", Size = DuckSizes.Error });
+            }
+            else
+            {
+                LoadDucks();
+            }
+            DuckListView.ItemsSource = ducks;
+        }
+
+
+        private void LoadDucksButton_Click(object sender, RoutedEventArgs e)
+        {
+            ducks.Clear();
+            LoadDucks();
+            DuckListView.Items.Refresh();
+        }
+
+        private void SaveDucksButton_Click(object sender, RoutedEventArgs e)
+        {
+            SaveDucks();
+        }
+
+        private void ClearDucksButton_Click(object sender, RoutedEventArgs e)
+        {
+            ducks.Clear();
+            DuckListView.Items.Refresh();
+        }
+
+        private void AddDuckButton_Click(object sender, RoutedEventArgs e)
+        {
+            string duckName = NameTextbox.Text;
+            string duckColour = ((ColourInfo)ColourComboBox.SelectedValue).ColourName;
+            DuckSizes duckSize = (DuckSizes) SizeComboBox.SelectedIndex;
+
+            ducks.Add(new Duck(duckName, duckColour, duckSize,0));
+
+            DuckListView.Items.Refresh();
+        }
+
+        void AddSizes()
+        {
+            SizeComboBox.ItemsSource = Enum.GetValues(typeof(DuckSizes));
+        }
+
+        void AddColours()
+        {
+            var color_query =
+                from PropertyInfo property in typeof(Colors).GetProperties()
+                orderby property.Name
+                //orderby ((Color)property.GetValue(null, null)).ToString()
+                select new ColourInfo(
+                    property.Name,
+                    (Color)property.GetValue(null, null));
+            ColourComboBox.ItemsSource = color_query;
+        }
+
     }
+
+    
 }
